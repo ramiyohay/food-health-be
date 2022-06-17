7ba821b6c98f32400ecda043852c1d086bd42b30
ramiyohay <ramiyohay1@gmail.com>
2022-06-17 12:16:48 +0300

    Add files via upload

---
+++ routes/main.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const service = require("../services/payments-service");
+
+/* GET payments quality check. */
+router.get("/ping", async function (req, res, next) {
+  res.status(200).send("PONG!");
+});
+
+module.exports = router;
