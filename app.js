edaeb33ccfe011261417bd4e95c82bf046216386
ramiyohay <ramiyohay1@gmail.com>
2022-06-17 14:29:54 +0300

    added limiter

--- app.js
+++ app.js
@@ -2,17 +2,29 @@ const createError = require("http-errors");
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
 
 const mainRouter = require("./routes/main");
 
 const app = express();
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(helmet());
+app.disable("x-powered-by");
 
 // routes
+app.use("/api", limiter);
 app.use("/api", mainRouter);
 
 // catch 404 and forward to error handler
@@ -28,6 +40,7 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
+  res.send("Server error!");
   //res.render("error");
 });
 
